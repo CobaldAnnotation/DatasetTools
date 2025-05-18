6194d45d4be62293acb3165664ee8bbc70723289
houcha <ilia.baiuk.hu@gmail.com>
2025-05-18 10:46:08 +0200

    Fix generator call and add dataset filtering

--- build_and_publish.py
+++ build_and_publish.py
@@ -9,6 +9,17 @@ def main():
     parser = argparse.ArgumentParser(description="Load and process a CONLLU file")
 
     parser.add_argument(
+        "repo_id",
+        type=str,
+        help="huggingface repo to push built dataset to"
+    )
+    parser.add_argument(
+        "--config_name",
+        type=str,
+        default="default",
+        help='dataset configuration name within the repo, e.g. "en"'
+    )
+    parser.add_argument(
         "--train_data_path",
         type=str,
         help="path to the train conllu file"
@@ -24,23 +35,13 @@ def main():
         help="path to the optional test conllu file"
     )
     parser.add_argument(
-        "repo_id",
-        type=str,
-        help="huggingface repo to push built dataset to"
-    )
-    parser.add_argument(
-        "config_name",
-        type=str,
-        help='dataset configuration name within the repo, e.g. "en"'
-    )
-    parser.add_argument(
         "--tags",
         nargs="+",
         type=str,
         default=OPTIONAL_TAGS,
         choices=OPTIONAL_TAGS,
         help=(
-            "Tags to include in dataset, e.g. [heads, deprels, deps]."
+            "Tags to include in dataset, e.g. `heads deprels deps`."
             "By default, all CoBaLD tags are used."
         )
     )
@@ -50,19 +51,25 @@ def main():
     dataset_dict = DatasetDict()
 
     if args.train_data_path:
-        train_sentences = parse_incr(args.train_data_path, args.tags)
-        dataset_dict['train'] = Dataset.from_generator(lambda: train_sentences)
+        dataset_dict['train'] = Dataset.from_generator(
+            lambda: parse_incr(args.train_data_path, args.tags)
+        )
 
     if args.validation_data_path:
-        validation_sentences = parse_incr(args.validation_data_path, args.tags)
-        dataset_dict['validation'] = Dataset.from_generator(lambda: validation_sentences)
+        dataset_dict['validation'] = Dataset.from_generator(
+            lambda: parse_incr(args.validation_data_path, args.tags)
+        )
 
     if args.test_data_path:
-        test_sentences = parse_incr(args.test_data_path, args.tags)
-        dataset_dict['test'] = Dataset.from_generator(lambda: test_sentences)
-    
-    dataset_dict.push_to_hub(args.repo_id, args.config_name)
+        dataset_dict['test'] = Dataset.from_generator(
+            lambda: parse_incr(args.test_data_path, args.tags)
+        )
 
+    # Add custom filtering here if needed.
+    dataset_dict.filter(lambda x: 3 <= len(x["id"]))
+    
+    # Push dataset to hub.
+    dataset_dict.push_to_hub(args.repo_id, args.config_name)
 
 if __name__ == "__main__":
     main()
